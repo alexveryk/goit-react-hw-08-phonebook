d3ffc87fe764b9cabf37a1ad1850228cc3fdc910
alexveryk <veryk.av@gmail.com>
2023-04-17 19:25:30 +0300

    Create ContactList.jsx

---
+++ src/components/ContactLst/ContactList.jsx
@@ -0,0 +1,33 @@
+import { List } from './ContactList.styled';
+import { ListItem } from './ContactList.styled';
+import { BtnDelete } from './ContactList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectVisibleName } from 'redux/selectors';
+import { deleteContacts } from 'redux/operations';
+
+export const ContactLst = () => {
+  const dispatch = useDispatch();
+  const handleDelete = id => dispatch(deleteContacts(id));
+
+  const visibleName = useSelector(selectVisibleName);
+
+  return (
+    <List>
+      {visibleName.map(contact => {
+        return (
+          <ListItem key={contact.id}>
+            {contact.name}: {contact.number}
+            <BtnDelete
+              type="button"
+              onClick={() => {
+                handleDelete(contact.id);
+              }}
+            >
+              Delete
+            </BtnDelete>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
