eb14893aaecda9fe917769738b3adc2c0b33928c
alexveryk <veryk.av@gmail.com>
2023-04-17 19:25:36 +0300

    Create Filter.styled.jsx

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 5px;
+`;
+
+export const FilterLabel = styled.label`
+  font-weight: 600;
+`;
+
+export const FilterInput = styled.input`
+  height: 28px;
+  font-size: 16px;
+`;
