85f39a4813626a37f94ecd34daee74c374d61660
alexveryk <veryk.av@gmail.com>
2023-04-17 19:25:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,29 @@
+import { ContactLst } from './ContactLst/ContactList';
+import { Filter } from './Filter/Filter';
+import { PhonebookForm } from './PhonebookForm/PhoneForm';
+import { AppContainer } from './App.styled';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
+
 export const App = () => {
-  return <App></App>;
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <AppContainer>
+      <h1>Phonebook</h1>
+      <PhonebookForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactLst />
+    </AppContainer>
+  );
 };
