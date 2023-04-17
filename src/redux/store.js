d5f6c5f7a4125259c5fffea1d7d817dea3ebba13
alexveryk <veryk.av@gmail.com>
2023-04-17 19:25:56 +0300

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { filterReducer } from './filterSlice';
+import { contactsReducer } from './contactsSlice';
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
+
+export default store;
