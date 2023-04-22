e1bf92ac61e6fcfb17a5efec096791cc2105c7cf
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:58 +0300

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,6 @@ import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 export const Register = () => {
   return (
     <div>
-      <h1>Register Page</h1>
       <RegisterForm />
     </div>
   );
