1fb261b1d9ec8acfa1c20b8a61ea5cc27eecb098
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:16 +0300

    Update ContactList.jsx

--- src/components/ContactLst/ContactList.jsx
+++ src/components/ContactLst/ContactList.jsx
@@ -1,9 +1,19 @@
-import { List } from './ContactList.styled';
-import { ListItem } from './ContactList.styled';
-import { BtnDelete } from './ContactList.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectVisibleName } from 'redux/contacts/selectors';
 import { deleteContacts } from 'redux/contacts/operations';
+import {
+  Card,
+  Stack,
+  CardBody,
+  Flex,
+  Box,
+  Spacer,
+  Heading,
+  Button,
+  Avatar,
+  List,
+  ListItem,
+} from '@chakra-ui/react';
 
 export const ContactLst = () => {
   const dispatch = useDispatch();
@@ -15,8 +25,44 @@ export const ContactLst = () => {
     <List>
       {visibleName.map(contact => {
         return (
-          <ListItem key={contact.id}>
-            {contact.name}: {contact.number}
+          <ListItem key={contact.id} mb="16px">
+            <Card size="md" w="100%">
+              <CardBody alignItems="center">
+                <Flex alignItems="center">
+                  <Box p="2">
+                    <Heading as="h4" size="md">
+                      <Stack direction="row">
+                        <Avatar
+                          name={contact.name}
+                          src="https://bit.ly/broken-link"
+                        />
+                      </Stack>
+                    </Heading>
+                  </Box>
+                  <Box p="2">
+                    <Heading as="h4" size="md">
+                      {contact.name}: {contact.number}
+                    </Heading>
+                  </Box>
+                  <Spacer />
+                  <Box p="2">
+                    <Button
+                      colorScheme="red"
+                      size="md"
+                      type="button"
+                      onClick={() => {
+                        handleDelete(contact.id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                    {/* <BtnDelete>Delete</BtnDelete> */}
+                  </Box>
+                </Flex>
+              </CardBody>
+            </Card>
+
+            {/* {contact.name}: {contact.number}
             <BtnDelete
               type="button"
               onClick={() => {
@@ -24,7 +70,7 @@ export const ContactLst = () => {
               }}
             >
               Delete
-            </BtnDelete>
+            </BtnDelete> */}
           </ListItem>
         );
       })}
