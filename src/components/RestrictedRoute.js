22d2b4c1a6900cd8d250f9d36d10b8c462f3b172
alexveryk <veryk.av@gmail.com>
2023-04-22 22:08:26 +0300

    Create RestrictedRoute.js

---
+++ src/components/RestrictedRoute.js
@@ -0,0 +1,9 @@
+import { useAuth } from 'hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const { isLoggedIn } = useAuth();
+  console.log(isLoggedIn);
+
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
