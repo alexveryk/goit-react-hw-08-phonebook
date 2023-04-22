00dfc074c4a61b424b238e6231d64fae52258d54
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:14 +0300

    Create AuthNav.styled.js

---
+++ src/components/AuthNav/AuthNav.styled.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const StyledLink = styled(NavLink)`
+  color: black;
+  font-size: 18px;
+  font-weight: 700;
+
+  &.active {
+    color: #276749;
+  }
+  &:nth-child(1) {
+    margin-right: 16px;
+  }
+`;
