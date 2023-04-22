f43e5137c24f443841f765b5443da60a3c887785
alexveryk <veryk.av@gmail.com>
2023-04-22 22:08:33 +0300

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,3 +1,26 @@
+import { ContactLst } from 'components/ContactLst/ContactList';
+import { Filter } from 'components/Filter/Filter';
+
+import { PhonebookForm } from 'components/PhonebookForm/PhoneForm';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectIsLoading } from 'redux/contacts/selectors';
+
 export const Contacts = () => {
-  return <div>Contacts Page</div>;
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <PhonebookForm />
+      <Filter />
+      <div>{isLoading && 'Request in progress...'}</div>
+      <ContactLst />
+    </div>
+  );
 };
