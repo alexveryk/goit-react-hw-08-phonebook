6e0ff1d58de36a3015616f9ba56bb6567ca692d2
alexveryk <veryk.av@gmail.com>
2023-04-22 22:08:24 +0300

    Create RegisterForm.js

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,93 @@
+import { Formik, Field } from 'formik';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  VStack,
+} from '@chakra-ui/react';
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
+
+export const RegisterForm = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <Flex bg="gray.100" align="center" justify="center" h="100vh">
+      <Box bg="white" p={6} rounded="md" w={64}>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+            name: '',
+          }}
+          onSubmit={(values, actions) => {
+            console.log(values);
+            dispatch(
+              register({
+                email: values.email,
+                password: values.password,
+                name: values.name,
+              })
+            );
+            actions.resetForm();
+          }}
+        >
+          {({ handleSubmit, errors, touched }) => (
+            <form onSubmit={handleSubmit}>
+              <VStack spacing={4} align="flex-start">
+                <FormControl>
+                  <FormLabel htmlFor="name"> User Name</FormLabel>
+                  <Field
+                    as={Input}
+                    id="name"
+                    name="name"
+                    type="text"
+                    variant="filled"
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="email">Email Address</FormLabel>
+                  <Field
+                    as={Input}
+                    id="email"
+                    name="email"
+                    type="email"
+                    variant="filled"
+                  />
+                </FormControl>
+                <FormControl isInvalid={!!errors.password && touched.password}>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Field
+                    as={Input}
+                    id="password"
+                    name="password"
+                    type="password"
+                    variant="filled"
+                    validate={value => {
+                      let error;
+
+                      if (value.length < 6) {
+                        error = 'Password must contain at least 6 characters';
+                      }
+
+                      return error;
+                    }}
+                  />
+                  <FormErrorMessage>{errors.password}</FormErrorMessage>
+                </FormControl>
+
+                <Button type="submit" colorScheme="purple" width="full">
+                  Register
+                </Button>
+              </VStack>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </Flex>
+  );
+};
