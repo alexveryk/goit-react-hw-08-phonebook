55c6aa0933c19778f068893c656e147e97064cc7
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:30 +0300

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import { Tabs, TabList, Tab } from '@chakra-ui/react';
+import { StyledLink } from './Navigation.styled';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+      <Tabs variant="soft-rounded" colorScheme="green">
+        <TabList>
+          <StyledLink to="/">Home</StyledLink>
+
+          {isLoggedIn && <StyledLink to="/contacts">Contacts</StyledLink>}
+        </TabList>
+      </Tabs>
     </nav>
   );
 };
