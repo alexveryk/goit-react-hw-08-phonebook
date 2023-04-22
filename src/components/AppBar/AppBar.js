68eae6bedb618066829fd5444c53beae989ef2fb
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:09 +0300

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,14 +2,21 @@ import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { Navigation } from '../Navigation/Navigation';
 import { useAuth } from '.././/../hooks/useAuth';
+import { Flex, Box, Spacer, Divider } from '@chakra-ui/react';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <header>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      <Flex alignItems="center">
+        <Box p="4">
+          <Navigation />
+        </Box>
+        <Spacer />
+        <Box p="4">{isLoggedIn ? <UserMenu /> : <AuthNav />}</Box>
+      </Flex>
+      <Divider mb="20px" />
     </header>
   );
 };
