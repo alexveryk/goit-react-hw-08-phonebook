6bb0c5dfe1fac3587c38a4d78a4b0d60ee090fd5
alexveryk <veryk.av@gmail.com>
2023-04-23 01:49:00 +0300

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -21,7 +21,6 @@ export const register = createAsyncThunk(
 
       return response.data;
     } catch (error) {
-      console.log(error);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -56,7 +55,6 @@ export const refreshUser = createAsyncThunk(
   async (_, thunkAPI) => {
     const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
-    console.log('persistedToken', persistedToken);
 
     if (persistedToken === null) {
       return thunkAPI.rejectWithValue('Unable to fetch user');
