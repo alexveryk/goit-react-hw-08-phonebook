b89eec13fc07fe523447bda081787027e1a14a6d
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:50 +0300

    Update UserMenu.js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/operations';
 import { useAuth } from '../../hooks/useAuth';
+import { Flex, Spacer, Box, Stack, Button } from '@chakra-ui/react';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -9,11 +10,21 @@ export const UserMenu = () => {
   const handleLogOut = () => dispatch(logOut());
 
   return (
-    <div>
-      <p>Welcome, {user.name}</p>
-      <button type="button" onClick={handleLogOut}>
-        Logout
-      </button>
-    </div>
+    <Flex alignItems="center">
+      <Box p="2">Welcome, {user.name}</Box>
+      <Spacer />
+      <Box p="2">
+        <Stack direction="row" spacing={4} align="center">
+          <Button
+            colorScheme="teal"
+            variant="ghost"
+            type="button"
+            onClick={handleLogOut}
+          >
+            Logout
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
   );
 };
