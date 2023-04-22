5db6a4c0b92236c479ddda13ca910586cecc8733
alexveryk <veryk.av@gmail.com>
2023-04-22 22:08:16 +0300

    Create LoginFofm.js

---
+++ src/components/LoginForm/LoginFofm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+} from '@chakra-ui/react';
+import { useDispatch } from 'react-redux';
+import { logIn } from 'redux/auth/operations';
+
+export const LoginForm = () => {
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values, actions) => {
+      dispatch(logIn(values));
+      actions.resetForm();
+    },
+  });
+  return (
+    <Flex bg="gray.100" align="center" justify="center" h="100vh">
+      <Box bg="white" p={6} rounded="md">
+        <form onSubmit={formik.handleSubmit}>
+          <VStack spacing={4} align="flex-start">
+            <FormControl>
+              <FormLabel htmlFor="email">Email Address</FormLabel>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                variant="filled"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                variant="filled"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+            </FormControl>
+
+            <Button type="submit" colorScheme="purple" width="full">
+              Login
+            </Button>
+          </VStack>
+        </form>
+      </Box>
+    </Flex>
+  );
+};
+
+// /input type="email" id="email"
