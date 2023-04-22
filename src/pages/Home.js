f2937773846e691f87f28c65a8c86ac938b43444
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:54 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,35 @@
+import {
+  Center,
+  Heading,
+  Card,
+  CardHeader,
+  Button,
+  CardBody,
+  Image,
+  CardFooter,
+} from '@chakra-ui/react';
+
 export const Home = () => {
   return (
     <div>
-      <h1>Welocome to phonebook</h1>
+      <Center color="black">
+        <Card align="center">
+          <CardHeader>
+            <Heading size="md">
+              Phonebook is an application for saving your contacts
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Image
+              src="gibbresh.png"
+              fallbackSrc="https://images.pexels.com/photos/6915114/pexels-photo-6915114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+          </CardBody>
+          {/* <CardFooter>
+            <Button colorScheme="blue">View here</Button>
+          </CardFooter> */}
+        </Card>
+      </Center>
     </div>
   );
 };
