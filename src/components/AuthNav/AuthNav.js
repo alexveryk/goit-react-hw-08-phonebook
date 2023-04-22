b7185e4191b731cb47454322a01c8834bc133fd7
alexveryk <veryk.av@gmail.com>
2023-04-23 01:48:11 +0300

    Update AuthNav.js

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,10 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import { Tabs, TabList, Tab } from '@chakra-ui/react';
+import { StyledLink } from './AuthNav.styled';
 
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Log In</NavLink>
+      <Tabs variant="soft-rounded" colorScheme="green">
+        <TabList>
+          <StyledLink to="/register">Register</StyledLink>
+
+          <StyledLink to="/login">Log In</StyledLink>
+        </TabList>
+      </Tabs>
     </div>
   );
 };
